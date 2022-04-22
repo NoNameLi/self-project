2a25c993350e2241103ae57e038ce7cb4f7053d7
y159632957@qq.com <Lcp159632957>
2022-04-22 21:49:48 +0800

    重命名

--- studygodpath/src/main/java/cn/peng/studygodpath/algorithm/leetcode/dp/LeetCode139StrMadeArrayElement.java
+++ studygodpath/src/main/java/cn/peng/studygodpath/algorithm/leetcode/dp/LeetCode139StrMadeArrayElement.java
@@ -4,7 +4,14 @@ import org.testng.collections.Lists;
 
 import java.util.List;
 
-public class StrMadeArrayElement {
+/**
+ * 给你一个字符串 s 和一个字符串列表 wordDict 作为字典。请你判断是否可以利用字典中出现的单词拼接出 s 。
+ *
+ * 注意：不要求字典中出现的单词全部都使用，并且字典中的单词可以重复使用。
+ *
+ * 链接：https://leetcode-cn.com/problems/word-break
+ */
+public class LeetCode139StrMadeArrayElement {
 
     public static boolean checkStr(String s, List<String> wordDict) {
         int length = s.length();
