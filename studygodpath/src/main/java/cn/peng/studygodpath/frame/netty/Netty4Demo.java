3e171f23059b6ff35a564f5f6975956a65c679c9
李超鹏 <licp@weixiuhui.cn>
2022-05-28 10:50:14 +0800

    netty3 demo netty4 demo

---
+++ studygodpath/src/main/java/cn/peng/studygodpath/frame/netty/Netty4Demo.java
@@ -0,0 +1,31 @@
+package cn.peng.studygodpath.frame.Netty;
+
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class Netty4Demo {
+
+    public static void main(String[] args) throws InterruptedException {
+        ServerBootstrap server = new ServerBootstrap();
+        EventLoopGroup boosGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup(10);
+        server.group(boosGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    @Override
+                    protected void initChannel(NioSocketChannel ch) throws Exception {
+                        ch.pipeline().addLast(new cn.peng.studygodpath.frame.netty.handler.DiscardServerHandler());
+                    }
+                }).option(ChannelOption.SO_BACKLOG, 128)
+                .childOption(ChannelOption.SO_KEEPALIVE, true);
+        server.bind(1000).sync();
+    }
+
+
+}
