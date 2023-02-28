66101bf2fb54e0c048d3e9f6461a0fbd237bb3a6
李超鹏 <licp@weixiuhui.cn>
2023-02-28 10:12:45 +0800

    ThreadLocal 内存泄露原因

--- studygodpath/src/main/java/cn/peng/studygodpath/java8/thread/ThreadLocalTest.java
+++ studygodpath/src/main/java/cn/peng/studygodpath/java8/thread/ThreadLocalTest.java
@@ -1,6 +1,9 @@
 package cn.peng.studygodpath.java8.thread;
 
 import java.lang.ref.WeakReference;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Stream;
 
 /**
@@ -9,10 +12,10 @@ import java.util.stream.Stream;
  * put方法，会以 threadLocal对象为key 参数为值 存入当前线程的 threadLocalMaps 中，其中 key是以 弱引用引用的，
  * 这样 threadLocal对象 会有两个引用，即：new 时的引用，和 threadLocalMaps里的key 弱引用，
  * 当将new引用置为空时，jvm会将 threadLocalMaps 中的key回收，这样当线程销毁前，对应的值就不能被访问 不能垃圾回收
- *
+ * <p>
  * 解决：
- *  1.使用 remove 方法 也可以 set get 方法会清除无用entity
- *  2.线程销毁
+ * 1.使用 remove 方法 也可以 set get 方法会清除无用entity
+ * 2.线程销毁
  */
 public class ThreadLocalTest {
 
@@ -24,6 +27,11 @@ public class ThreadLocalTest {
         System.gc();
         Thread thread = Thread.currentThread();
         WeakReference<Long> reference = new WeakReference<>(0L);
+
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 20, 60, TimeUnit.MINUTES
+                , new ArrayBlockingQueue<>(50), (runnable) -> {
+            return new Thread(runnable);
+        }, new ThreadPoolExecutor.AbortPolicy());
     }
 
 
