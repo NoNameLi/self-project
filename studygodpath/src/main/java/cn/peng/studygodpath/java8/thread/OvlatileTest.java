4857c43b9aa5f31f78c8735a08568444b58b250b
李超鹏 <licp@weixiuhui.cn>
2023-03-31 15:24:33 +0800

    volatile 保证了 可见性 有序性 但不能保证原子性

--- studygodpath/src/main/java/cn/peng/studygodpath/java8/thread/OvlatileTest.java
+++ studygodpath/src/main/java/cn/peng/studygodpath/java8/thread/OvlatileTest.java
@@ -1,21 +1,74 @@
 package cn.peng.studygodpath.java8.thread;
 
+import com.google.common.collect.Lists;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 public class OvlatileTest {
 
     public static volatile Boolean stop = false;
 
-    public static void main(String[] args) throws Exception{
+    private static volatile int num = 0;
+
+    @Test
+    public void testVolatileVisibility() throws InterruptedException {
+        Thread writeThread = new Thread(() -> {
+            for (int i = 0; i < 100; i++) {
+                num++;
+                try {
+                    TimeUnit.MILLISECONDS.sleep(50);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        Thread readThread = new Thread(() -> {
+            while (num < 100 - 1) {
+            }
+            System.out.println(Thread.currentThread().getName() + " read:" + num);
+        });
+        writeThread.start();
+        readThread.start();
+        readThread.join();
+    }
+
+
+    @Test
+    public void testVolatileIntAdd() throws InterruptedException {
+        Runnable runnable = () -> {
+            for (int i = 0; i < 1000; i++) {
+                num = num + 1;
+            }
+        };
+
+        List<Thread> threadList = Lists.newArrayListWithCapacity(20);
+        for (int i = 0; i < 20; i++) {
+            threadList.add(new Thread(runnable));
+        }
+        for (int i = 0; i < 20; i++) {
+            threadList.get(i).start();
+        }
+        for (int i = 0; i < 20; i++) {
+            threadList.get(i).join();
+        }
+        Assert.assertEquals(num, 1000 * 20);
+    }
+
+    public static void main(String[] args) throws Exception {
 //        test();
         test2();
     }
 
-    public static void test2()throws Exception{
-        Thread watch = new Thread(()->{
+    public static void test2() throws Exception {
+        Thread watch = new Thread(() -> {
             while (!stop) {
             }
-            System.out.println(Thread.currentThread() +":" + stop);
+            System.out.println(Thread.currentThread() + ":" + stop);
         });
-        Thread change = new Thread(()->{
+        Thread change = new Thread(() -> {
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
@@ -28,7 +81,7 @@ public class OvlatileTest {
         watch.join();
     }
 
-    public static void test() throws Exception{
+    public static void test() throws Exception {
         Thread thread = new Thread(() -> {
             int i = 0;
             while (!stop) {
