283591ad4811b163942c7b60948a2313169e2482
李超鹏 <licp@weixiuhui.cn>
2023-03-31 17:06:54 +0800

    sleep 阻塞不会释放锁占用 wait 会释放锁

--- studygodpath/src/main/java/cn/peng/studygodpath/java8/thread/SleepUtil.java
+++ studygodpath/src/main/java/cn/peng/studygodpath/java8/thread/SleepUtil.java
@@ -1,5 +1,7 @@
 package cn.peng.studygodpath.java8.thread;
 
+import org.testng.annotations.Test;
+
 import java.util.concurrent.ThreadLocalRandom;
 
 public class SleepUtil {
@@ -11,6 +13,48 @@ public class SleepUtil {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    public void sleepTest() throws InterruptedException {
+        Object lock = new Object();
+
+        Thread thread = new Thread(() -> {
+            synchronized (lock) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                lock.notifyAll();
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    Thread.sleep(4000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(Thread.currentThread().getName() + " finish");
+            }
+        });
+        Thread thread1 = new Thread(() -> {
+            synchronized (lock) {
+                System.out.println(Thread.currentThread().getName() + " finish");
+                lock.notifyAll();
+            }
+        });
+        thread.start();
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        thread1.start();
+        thread1.join();
+        thread.join();
     }
 
 
